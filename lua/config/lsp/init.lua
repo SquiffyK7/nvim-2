6f2724c57004a49e28e5d7d59c17f6170a6aef8e
Lewis Kimber <lewis.kimber@caplin.com>
2023-10-06 08:23:11 +0100

    Telescope live_grep includes hidden files. Add schemastore and enable for json and yaml files.

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -37,6 +37,27 @@ local servers = {
       })
     end,
   },
+  jsonls = {
+    settings = {
+      json = {
+        schemas = require('schemastore').json.schemas(),
+      },
+    },
+  },
+  yamlls = {
+    settings = {
+      yaml = {
+        schemaStore = {
+          -- You must disable built-in schemaStore support if you want to use
+          -- this plugin and its advanced options like `ignore`.
+          enable = false,
+          -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+          url = '',
+        },
+        schemas = require('schemastore').yaml.schemas(),
+      },
+    },
+  },
 }
 
 function M.setup()
--- lua/config/telescope/init.lua
+++ lua/config/telescope/init.lua
@@ -28,6 +28,12 @@ function M.setup()
           },
         },
       },
+
+      live_grep = {
+        additional_args = function(opts)
+          return { '--hidden' }
+        end,
+      },
     },
   }
 
